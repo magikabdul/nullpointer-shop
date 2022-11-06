d7ed935c268a4e47fa387f9ddd26d6c35b550714
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-11-06 23:35:48 +0100

    [SHOP] initial application

---
+++ src/test/java/cloud/cholewa/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
